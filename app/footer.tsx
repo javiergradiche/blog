251245e5687296630ce51fd08fbc0547fa935fb5
javiergradiche <javiergradiche@gmail.com>
2023-05-16 11:09:09 -0300

    add coworkear and name

--- app/footer.tsx
+++ app/footer.tsx
@@ -3,17 +3,23 @@ import { A } from "./(post)/components/a";
 export function Footer() {
   return (
     <footer className="p-6 pt-3 pb-6 flex text-xs text-center mt-3 dark:text-gray-400 text-gray-500 font-mono">
-      <div className="grow text-left">
-        Javier Gradiche (
-        <A target="_blank" href="https://twitter.com/javiergradiche">
-          @javiergradiche
-        </A>
-        )
+      <div className="grow text-left w-1/2">
+        <div className="flex flex-col">
+          <A target="_blank" href="https://twitter.com/javiergradiche" className="no-underline">
+            Twitter: @javiergradiche
+          </A>
+          <A target="_blank" href="mailto:javiergradiche@gmail.com" className="no-underline">
+            Mail: javiergradiche@gmail.com
+          </A>
+          </div>
       </div>
-      <div>
-        <A target="_blank" href="https://github.com/javiergradiche/blog">
-          Source
-        </A>
+      <div className="flex flex-col text-right w-1/2">
+        <div>
+          Este sitio web es propiedad de y es administrado por Javier Gradiche
+        </div>
+        <div>
+        This website is owned by and managed by Javier Gradiche
+        </div>
       </div>
     </footer>
   );
